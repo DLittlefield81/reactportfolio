a70fe4ed0167786c99aea9db95ce9839e1355caf
DLittlefield81 <dennis.ah.littlefield@gmail.com>
2022-07-19 17:52:38 -0400

    added MUI and have a header working

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { AppBar, Typography } from '@mui/material'
+const Header = () => {
+    return (
+        <React.Fragment>
+            <AppBar>
+                <Typography>
+
+                    Dennis Littlefield Profile
+                </Typography>
+            </AppBar>
+        </React.Fragment>
+    )
+}
+
+export default Header
